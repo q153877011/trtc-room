4a0d86e83d0b35846a0a4d08eff37523e8b75566
wenyiqing <wenyiqing@tencent.com>
2025-04-11 15:18:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { UIKitProvider } from '@tencentcloud/uikit-base-component-vue3';
+</script>
+
+<template>
+  <UIKitProvider :theme="{ themeStyle: 'dark', primaryColor: 'theme' }">
+    <router-view></router-view>
+  </UIKitProvider>
+</template>
+
+<style lang="scss">
+  html,body,#app{
+        height: 100%;
+    }
+  #app {
+    overflow: hidden;
+  }
+</style>
